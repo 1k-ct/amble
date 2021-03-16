acee74ce9ee226c004644f57d2dfdd2d3737ba79
1k-ct <268iop@gmail.com>
2021-03-16 20:57:44 +0900

    init

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import React from "react";
+import TopBar from "./components/TopBar";
+import CenterGrid from "./components/LayoutGrid";
+function App() {
+  return (
+    <div className="App">
+      <CenterGrid></CenterGrid>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/TopBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const TopBar: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default TopBar;
