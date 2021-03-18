2518e08cb78638337740fdd8a301317566944aba
1k-ct <268iop@gmail.com>
2021-03-18 23:49:23 +0900

    大きいcontainerの中にgridを入れた　比(2, 7, 3)

--- front/src/components/LayoutGrid.tsx
+++ front/src/components/LayoutGrid.tsx
@@ -3,7 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import SimpleList from "./SimpleList";
-
+import TweetList from "./TweetList";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -19,20 +21,26 @@ const CenteredGrid: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={2}>
-          {/* <Paper className={classes.paper}>xs=3</Paper> */}
-          <SimpleList />
+    <Container>
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs={2}>
+            <SimpleList />
+            <Typography
+              component="div"
+              style={{ backgroundColor: "#cfe8fc", height: "100%" }}
+            />
+          </Grid>
+          <Grid item xs={7}>
+            {/* <Paper className={classes.paper}>xs=6</Paper> */}
+            <TweetList />
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={7}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Container>
   );
 };
 export default CenteredGrid;
