fc595e915fa5c351ab47a01850339f79775d65df
1k-ct <268iop@gmail.com>
2021-03-21 20:34:19 +0900

    model

---
+++ app/domain/model/tweet.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+type Tweet struct {
+	ID           int64  `gorm:"primary_key" json:"id,omitempty"`
+	Name         string `json:"name"`
+	Content      string `json:"content"`
+	LikeCount    int64  `json:"like_count"`
+	RetweetCount int64  `json:"retweet_count"`
+	// reply の lenでするかも
+	ReplyCount int64     `json:"reply_count"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	// TODO リプライも追加 reply content [] array ? list ?
+	// Replies []Reply
+	// hang from ぶらさがっているツイート (reply)
+}
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
