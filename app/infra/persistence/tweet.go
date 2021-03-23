5d0ee654793cc2c8ee8674e6a8085896c5d131d5
1k-ct <268iop@gmail.com>
2021-03-23 21:26:29 +0900

    インフラ層

--- app/infra/persistence/tweet.go
+++ app/infra/persistence/tweet.go
@@ -3,7 +3,10 @@ package persistence
 import (
 	"github.com/1k-ct/twitter-dem/app/domain/model"
 	"github.com/1k-ct/twitter-dem/app/domain/repository"
+	"github.com/1k-ct/twitter-dem/pkg/database"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 type tweetPersistence struct{}
@@ -11,12 +14,67 @@ type tweetPersistence struct{}
 func NewTweetPersistence() repository.TweetRepository {
 	return &tweetPersistence{}
 }
+func connect() (*gorm.DB, error) {
+	config, err := database.NewLocalDB("user", "password", "sample")
+	if err != nil {
+		return nil, err
+	}
 
-func (tp *tweetPersistence) RegisterTweet(c *gin.Context) (*model.Response, error) {
-	return nil, nil
+	db, err := config.Connect()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
-func (tp *tweetPersistence) GetTweetByIDs(c *gin.Context, id []int64) ([]*model.Tweet, error) {
-	return nil, nil
+func (tp *tweetPersistence) RegisterTweet(c *gin.Context, tweet *model.Tweet) error {
+	db, err := connect()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	if err := db.Create(&tweet).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+func (tp *tweetPersistence) GetTweetByID(c *gin.Context, id int64) (*model.Tweet, error) {
+	db, err := connect()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	// tweets := []*model.Tweet{}
+	tweet := &model.Tweet{}
+	// for _, id := range ids {
+	// 	if err := db.Where("id = ?", id).Find(&tweet).Error; err != nil {
+	// 		return nil, err
+	// 	}
+	// 	tweets = append(tweets, tweet)
+	// 	log.Println(tweet)
+	// 	log.Println(&tweets)
+	// }
+
+	if err := db.Where("id = ?", id).Find(&tweet).Error; err != nil {
+		return nil, err
+	}
+	return tweet, nil
+}
+func (tp *tweetPersistence) GetTweetByIDLimitOffset(c *gin.Context, id, limit, offset int64) ([]*model.Tweet, error) {
+	db, err := connect()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	tweet := []*model.Tweet{}
+	if err := db.Limit(limit).Offset(offset).Find(&tweet).Error; err != nil {
+		return nil, err
+	}
+
+	return tweet, nil
 }
 func (tp *tweetPersistence) DeleteTweetByID(c *gin.Context, id int64) (*model.Response, error) {
 	return nil, nil
---
+++ app/infra/persistence/tweet_test.go
@@ -0,0 +1,66 @@
+package persistence
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/1k-ct/twitter-dem/app/domain/model"
+	"github.com/1k-ct/twitter-dem/pkg/database"
+	"github.com/gin-gonic/gin"
+)
+
+func TestRegisterTweet(t *testing.T) {
+	db, err := database.NewLocalDB("user", "password", "sample")
+	if err != nil {
+		t.Error(err)
+	}
+	if err := db.NewMakeDB(&model.Tweet{}); err != nil {
+		t.Error(err)
+	}
+
+	// db, err := connect()
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+	// twe := &model.Tweet{}
+	// db.AutoMigrate(twe)
+
+	tweet := &model.Tweet{
+		// ID:           0,
+		Name:         "sato",
+		Content:      "テストのでーたツイート内容",
+		LikeCount:    0,
+		RetweetCount: 0,
+		ReplyCount:   0,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	var c *gin.Context
+	if err := NewTweetPersistence().RegisterTweet(c, tweet); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetTweetByIDs(t *testing.T) {
+	var c *gin.Context
+	tweet, err := NewTweetPersistence().GetTweetByID(c, 2)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(tweet)
+}
+
+func TestGetTweetByIDLimitOffset(t *testing.T) {
+	var c *gin.Context
+	tweets, err := NewTweetPersistence().GetTweetByIDLimitOffset(c, 2, 3, 3)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, tweet := range tweets {
+		fmt.Println(tweet)
+	}
+	// log.Println(tweets)
+}
