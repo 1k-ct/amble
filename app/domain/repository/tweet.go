83ba774f6ca9e97a0001fd38eb4719ee2fdd1c53
1k-ct <268iop@gmail.com>
2021-03-24 23:16:31 +0900

    複数取得できるように

--- app/domain/repository/tweet.go
+++ app/domain/repository/tweet.go
@@ -8,6 +8,6 @@ import (
 type TweetRepository interface {
 	RegisterTweet(c *gin.Context, tweet *model.Tweet) error
 	GetTweetByID(c *gin.Context, id int64) (*model.Tweet, error)
-	GetTweetByIDLimitOffset(c *gin.Context, id, limit, offset int64) ([]*model.Tweet, error)
-	DeleteTweetByID(c *gin.Context, id int64) (*model.Response, error)
+	GetTweetByIDs(c *gin.Context, ids []int64) ([]*model.Tweet, error)
+	DeleteTweetByID(c *gin.Context, id int64) error
 }
