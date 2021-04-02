0cd5edcabb7bbfe3f8c3c5691a2d003df4253b91
1k-ct <268iop@gmail.com>
2021-04-03 05:10:15 +0900

    ミドルウェア集

---
+++ pkg/middelware/jwtToken.go
@@ -0,0 +1,21 @@
+package middelware
+
+import (
+	"net/http"
+
+	"github.com/1k-ct/twitter-dem/pkg/appErrors"
+	"github.com/1k-ct/twitter-dem/pkg/jwtToken"
+	"github.com/gin-gonic/gin"
+)
+
+func TokenAuthMiddleware(secretKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := jwtToken.TokenValid(c.Request, secretKey)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, appErrors.ErrMeatdataMsg(err, appErrors.ErrInvalidToken))
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
