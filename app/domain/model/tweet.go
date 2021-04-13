77ff69dbecdf4144d3faeab670fdd935d11d5b79
1k-ct <268iop@gmail.com>
2021-04-13 11:53:08 +0900

    like retweet reply model　追加

--- app/domain/model/tweet.go
+++ app/domain/model/tweet.go
@@ -1,27 +1,31 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Tweet struct {
-	ID           int64     `gorm:"primary_key" json:"id"`
-	StaticID     int64     `json:"static_id"`
-	IsPrivate    bool      `json:"is_private"`
-	Name         string    `json:"name"`
-	Content      string    `json:"content"`
-	LikeCount    int64     `json:"like_count"`
-	IsLiked      bool      `json:"is_liked"`
-	RetweetCount int64     `json:"retweet_count"`
-	IsRetweeted  bool      `json:"is_retweeted"` // reply の lenでするかも
-	ReplyCount   int64     `json:"reply_count"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID             int64     `json:"id"`
+	UserStaticID   string    `json:"user_static_id"`
+	StaticID       string    `json:"static_id"`
+	IsPrivate      bool      `json:"is_private"`
+	Name           string    `json:"name"`
+	Content        string    `json:"content"`
+	LikedCount     int64     `json:"liked_count"`
+	RetweetedCount int64     `json:"retweeted_count"`
+	ReplyCount     int64     `json:"reply_count"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 	// TODO リプライも追加 reply content [] array ? list ?
 	// Replies []Reply
 	// hang from ぶらさがっているツイート (reply)
 }
 type Reply struct {
-	ID           int64     `gorm:"primary_key" json:"id"`
-	StaticID     int64     `json:"static_id"`
+	ToTweetID    string    `json:"to_tweet_id"`
+	UserStaticID string    `json:"user_static_id"`
+	StaticID     string    `json:"static_id"`
 	IsPrivate    bool      `json:"is_private"`
 	Name         string    `json:"name"`
 	Content      string    `json:"content"`
@@ -35,23 +39,21 @@ type Reply struct {
 }
 
 type LikedUser struct {
-	StaticID int64 `json:"static_id"`
-}
-type LikedUsers struct {
-	Users []LikedUser `json:"users"`
+	gorm.Model
+	ToTweetID    string `json:"to_tweet_id"`
+	UserStaticID string `json:"user_static_id"`
 }
 
 type RetweetedUser struct {
-	StaticID int64 `json:"static_id"`
-}
-type RetweetedUsers struct {
-	Users []RetweetedUser `json:"users"`
+	gorm.Model
+	ToTweetID    string `json:"to_tweet_id"`
+	UserStaticID string `json:"user_static_id"`
 }
 
 type Response struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int
+	Message string
 }
 type Request struct {
-	ID int64 `json:"id"`
+	ID int64
 }
