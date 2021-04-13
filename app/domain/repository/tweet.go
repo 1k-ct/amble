193b0c58542e43b216665f0308b10ef7fd87e0ac
1k-ct <268iop@gmail.com>
2021-04-13 11:52:29 +0900

    c *gin....削除

--- app/domain/repository/tweet.go
+++ app/domain/repository/tweet.go
@@ -2,12 +2,11 @@ package repository
 
 import (
 	"github.com/1k-ct/twitter-dem/app/domain/model"
-	"github.com/gin-gonic/gin"
 )
 
 type TweetRepository interface {
-	RegisterTweet(c *gin.Context, tweet *model.Tweet) error
-	GetTweetByID(c *gin.Context, id int64) (*model.Tweet, error)
-	GetTweetByIDs(c *gin.Context, ids []int64) ([]*model.Tweet, error)
-	DeleteTweetByID(c *gin.Context, id int64) error
+	RegisterTweet(tweet *model.Tweet) error
+	GetTweetByID(staticID string) (*model.Tweet, error)
+	GetTweetByIDs(ids []int64) ([]*model.Tweet, error)
+	DeleteTweetByID(id int64) error
 }
