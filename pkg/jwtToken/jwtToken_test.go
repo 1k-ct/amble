957a6f605d3558fef09d45c1edc7da13c99a84ca
1k-ct <268iop@gmail.com>
2021-07-21 12:32:23 +0900

    add jwt test

--- pkg/jwtToken/jwtToken_test.go
+++ pkg/jwtToken/jwtToken_test.go
@@ -2,6 +2,8 @@ package jwtToken
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"testing"
 
 	uuid "github.com/satori/go.uuid"
@@ -10,6 +12,10 @@ import (
 const (
 	privKeyPath = "keys/app.rsa"
 	pubKeyPath  = "keys/app.rsa.pub"
+	userID      = "testID"
+	userName    = "testName"
+	secretKey   = "testSecretKey"
+	refreshKey  = "testRefreshKey"
 )
 
 func TestUUID(t *testing.T) {
@@ -23,4 +29,44 @@ func TestUUID(t *testing.T) {
 		fmt.Println("error:", err)
 	}
 	fmt.Println("u2:", u2)
+}
+
+func TestCreateToken(t *testing.T) {
+	token := testCreateToken()
+	if token == "" {
+		t.Error("fatal create token")
+	}
+	fmt.Println(token)
+}
+func TestTokenValid(t *testing.T) {
+	token := testCreateToken()
+	if token == "" {
+		t.Error("fatal create token")
+	}
+
+	accessToken := fmt.Sprintf(`"access_token": %v`, token)
+	h := &http.Request{
+		Header: map[string][]string{"Authorization": {accessToken}},
+	}
+
+	jwtToken, err := VerifyToken(h, secretKey)
+	if err != nil {
+		t.Error("error verify token")
+	}
+	if !jwtToken.Valid {
+		t.Error("error token valid")
+	}
+
+	if err := TokenValid(h, secretKey); err != nil {
+		t.Error("error verify token")
+	}
+	fmt.Println("token ok!")
+}
+func testCreateToken() string {
+	got, err := CreateToken(userID, userName, secretKey, refreshKey)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+
+	return got.AccessToken
 }
